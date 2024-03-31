4921b85d69882dd0eafee0f702fb994aa91fd2ed
pradeepSharmadev <amarahipradeep7319@gmail.com>
2024-03-31 12:59:37 +0530

    front-end updated

--- day-2/appwithcra/front-end/src/App.js
+++ day-2/appwithcra/front-end/src/App.js
@@ -1,10 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/data")
+      .then((res) => {
+        console.log("data", res.data);
+        setDatas(res.data);
+      })
+      .catch((error) => {
+        console.log("error :", error);
+      });
+  }, []);
   return (
-    <>                
-    hello
+    <>
+      <h1>Contact Details</h1>
+      {datas.map((item, index) => {
+        return (
+          <div key={index}>
+            <p>Name:{item.name}</p>
+            <p>Email:{item.email}</p>
+            <button
+              onClick={() => {
+                alert("Delete this contact?");
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        );
+      })}
     </>
   );
 }
